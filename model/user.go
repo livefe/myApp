16287ae95bdd9706adbe39e5a93b669a4aeb708c
gaoshuang <gaoshuang@imcoming.cn>
2025-03-10 15:42:22 +0800

    #

--- model/user.go
+++ model/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	BaseModel
 	Username  string     `gorm:"uniqueIndex;size:50" json:"username"`
-	Password  string     `gorm:"size:100" json:"-"`
+	Password  string     `gorm:"size:100" json:"password,omitempty"`
 	Email     string     `gorm:"uniqueIndex;size:100" json:"email"`
 	Phone     string     `gorm:"size:20" json:"phone"`
 	Avatar    string     `gorm:"size:255" json:"avatar"`
